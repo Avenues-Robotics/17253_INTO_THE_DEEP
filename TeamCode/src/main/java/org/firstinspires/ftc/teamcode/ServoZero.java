f708828a33192b55874dbcfd5f6df7dbb9b86b7c
Noah-MG <noah.glimcher28@avenues.org>
2025-01-11 10:54:55 -0500

    Fixing the world

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoZero.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoZero.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
